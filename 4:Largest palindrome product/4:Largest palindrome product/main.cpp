ad9b28cecaab540030ff86036b0a03de8f334b1f
kiricon <kiricon@live.com>
2016-09-24 13:44:24 -0600

    Completed challenge 4 of project Euler!

---
+++ 4:Largest palindrome product/4:Largest palindrome product/main.cpp
@@ -0,0 +1,57 @@
+//
+//  main.cpp
+//  4:Largest palindrome product
+//
+//  Created by Dominic Valenciana on 9/24/16.
+//  Copyright © 2016 Dominic Valenciana. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+bool isPalin(int);
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    int max = 999;
+    vector<int> all;
+    
+    for(int i = max; i > 0; i--){
+        for(int x = max; x > 0 ; x--){
+            
+            int product = (i * x);
+            if(isPalin(product)){
+                all.push_back(product);
+            }
+            
+        }
+    }
+    
+    int largest = 0;
+    for(int i = 0; i < all.size(); i++){
+        if(all[i] > largest){
+            largest = all[i];
+        }
+    }
+    
+    cout << largest << '\n';
+     
+    //cout << isPalin(4804) << '\n';
+    return 0;
+}
+
+bool isPalin(int num){
+    
+    string original, palin;
+    
+    original = to_string(num);
+    palin = original;
+    reverse(palin.begin(), palin.end());
+    
+    //cout << original << " : "<< palin << '\n';
+    
+    return original == palin;
+}
