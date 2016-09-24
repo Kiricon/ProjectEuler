04abd3fa5cc7028afec690562046712759a8c1b4
kiricon <kiricon@live.com>
2016-09-24 11:27:03 -0600

    Setup first project to be ready

---
+++ 1:Multiples of 3 and 5/1:Multiples of 3 and 5/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  1:Multiples of 3 and 5
+//
+//  Created by Dominic Valenciana on 9/24/16.
+//  Copyright © 2016 Dominic Valenciana. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
