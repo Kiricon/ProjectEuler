3df4e3b4a0354ed6bf18bfe41b2c774988b1c0d5
kiricon <kiricon@live.com>
2016-09-26 21:03:33 -0600

    Removed the usless files

---
+++ 3:Largest prime factory/3:Largest prime factory/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  3:Largest prime factory
+//
+//  Created by Dominic Valenciana on 9/24/16.
+//  Copyright © 2016 Dominic Valenciana. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
