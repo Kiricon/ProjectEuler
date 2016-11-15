f544d96de4b67ade7b874e98660662c01cad87d6
kiricon <kiricon@live.com>
2016-11-14 19:58:48 -0700

    Completed challenge 14 of project euler

--- 14:Longest Collatz sequence/main.py
+++ 14:Longest Collatz sequence/main.py
@@ -4,15 +4,28 @@ def even(n):
 def odd(n):
     return 3*n+1
 
-num = 13
-i = 0
 
-while num != 1:
-    print num
-    if num % 2 == 0:
-        num = even(num)
-    else:
-        num = odd(num)
-    i += 1
-    
-print num
+def collatz(num):
+    i = 0
+    while num != 1:
+        if num % 2 == 0:
+            num = even(num)
+        else:
+            num = odd(num)
+        i += 1
+    return i
+
+largestNum = 0
+largestSequence = 0
+
+for n in range(1,1000000):
+
+    newNum = collatz(n)
+
+    if newNum > largestSequence:
+        largestSequence = newNum
+        largestNum = n
+
+
+print largestNum
+print largestSequence
